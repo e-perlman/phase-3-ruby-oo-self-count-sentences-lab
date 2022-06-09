a23ce119e518364c043841c34179b035a7c041f5
Emma Perlman <emmajperlman@gmail.com>
2022-06-09 11:12:42 -0700

    commit tree 6afc50bb8deaebe972eef23bf102330076d6088b

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    word_array=self.split(/[.!?]/).filter {|word| word.length>0}
+    word_array.length
   end
 end
+c=String.new('Hello.')
